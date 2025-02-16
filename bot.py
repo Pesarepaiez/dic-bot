b674fc4d8474e6e351c46fd99500ec3b0b0d3675
Momeni <mohammad51117momeni@gmail.com>
2025-02-16 13:17:34 +0430

    Updated bot with new features

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import re
 import os
 
 # Your Telegram Bot Token
-BOT_TOKEN = "5909441299:AAEv7WSNh2lrRFTa7gAhH9x8wzOembjmD94"
+BOT_TOKEN = "8058388234:AAEz9jW2tHlcbfyXC8daCC-rEnbxWzy4dLY"
 bot = telebot.TeleBot(BOT_TOKEN, parse_mode="HTML")  
 
 # Function to get word definition
